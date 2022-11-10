98af899192c325aa7fff56631a19dd48b9705caa
Armands Berzins <armands.berzins@me.com>
2022-11-10 21:41:00 +0200

    Inital

---
+++ Shortcutter/Shortcutter/ShortcutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutterApp.swift
+//  Shortcutter
+//
+//  Created by Armands Berzins on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
